67466cd977e28f49e883be867669f44cd991a4bf
b0wdyy <vandaelbodhi@hotmail.com>
2022-12-31 17:00:51 +0100

    chore: added env variables

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyAEU1HkMuv3evC_PT1RyxXDCucBAsMaST0',
-    authDomain: 'food-order-c11f9.firebaseapp.com',
-    projectId: 'food-order-c11f9',
-    storageBucket: 'food-order-c11f9.appspot.com',
-    messagingSenderId: '1058655504045',
-    appId: '1:1058655504045:web:9ad6b56f3e7c7744dde824',
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
 }
 
 export const app = initializeApp(firebaseConfig)
